a04a40211fd784ec163fec9eb1df4a45cf2054e8
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-08-01 08:05:26 +0330

    feat: Add core:network module
    This commit introduces the `core:network` module to the project.
    
    Key changes:
    - Added `core:network` module with a basic `MyClass.kt`.
    - Configured Gradle to include the new module in `settings.gradle.kts` and `.idea/gradle.xml`.
    - Added `.gitignore` for the `core/network` module.
    - Configured `core/network/build.gradle.kts` with library, network, and koin convention plugins.
    - Added a dependency on the `core.utils` project.

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    libs.plugins.coinNews.run {
+        alias(library)
+        alias(network)
+        alias(koin)
+    }
+}
+
+android {
+    namespace = "com.rahim.coinnews.core.network"
+}
+dependencies{
+    projects.run {
+//        implementation(core.db)
+        implementation(core.utils)
+    }
+}
---
+++ core/network/src/main/java/com/rahim/coinnews/network/MyClass.kt
@@ -0,0 +1,4 @@
+package com.rahim.coinnews.network
+
+class MyClass {
+}
