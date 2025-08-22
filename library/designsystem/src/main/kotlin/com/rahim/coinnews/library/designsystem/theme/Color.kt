8355850f7796f7f4dcd229222de12b3fea77b000
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-08-22 12:28:48 +0330

    feat: Add graph and trend colors
    This commit introduces new color definitions for graphs and price trends in the design system.
    
    Key changes in `library/designsystem/src/main/kotlin/com/rahim/coinnews/library/designsystem/theme/Color.kt`:
    - Added `graphColor` (primary graph color).
    - Added `lightGraphColor` (a lighter, semi-transparent version of `graphColor`).
    - Added `lightUptrendGreen` and `darkUptrendGreen` for positive price trends.
    - Added `lightDownTrendRed` and `darkDownTrendRed` for negative price trends.

--- library/designsystem/src/main/kotlin/com/rahim/coinnews/library/designsystem/theme/Color.kt
+++ library/designsystem/src/main/kotlin/com/rahim/coinnews/library/designsystem/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val graphColor = Color(0xFF6750A4)
+val lightGraphColor = graphColor.copy(alpha = 0.5f)
+val lightUptrendGreen = Color(0xFF4CAF50)
+val darkUptrendGreen = Color(0xFF00A86B)
+val lightDownTrendRed = Color(0xFFE50000)
+val darkDownTrendRed = Color(0xFFFF1919)
